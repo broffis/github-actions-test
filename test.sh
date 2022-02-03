baf3c8ac3e92508312a3ad88ab116a6e86f447c8
Brandon Roffis <brandon.roffis@ferguson.com>
2022-02-03 14:05:36 -0500

    mods to create diff

--- test.sh
+++ test.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-if [ true]
-then
-  curl \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/octocat/hello-world/statuses/SHA \
-  -d '{"state":"success"}'
-fi
+echo "this is a test file"
