25f27b9c41639be03252ab164a5de236dd7201c7
Brandon Roffis <brandon.roffis@ferguson.com>
2022-05-05 15:05:27 -0400

    testing context shas

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -1,4 +1,11 @@
 module.exports = async ({ github, context }) => {
-  console.log({ github });
-  console.log({ context });
+  // console.log({ github });
+  // console.log({ context });
+
+  const latestCommit = context.payload.after;
+  const previousCommit = context.payload.before;
+
+  console.log(
+    `Comparing latest commit ${latestCommit} with previous commit ${previousCommit} on ${context.ref}`
+  );
 };
