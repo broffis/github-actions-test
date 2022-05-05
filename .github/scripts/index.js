fde1732f14515248632ccc0de3f6fb144158b9fb
Brandon Roffis <brandon.roffis@ferguson.com>
2022-05-05 15:08:53 -0400

    testing ocktokit rest

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -2,10 +2,24 @@ module.exports = async ({ github, context }) => {
   // console.log({ github });
   // console.log({ context });
 
+  const {
+    name,
+    owner: { login },
+  } = context.payload.repository;
+
   const latestCommit = context.payload.after;
   const previousCommit = context.payload.before;
 
   console.log(
     `Comparing latest commit ${latestCommit} with previous commit ${previousCommit} on ${context.ref}`
   );
+
+  const compareCommits = await github.octokit.rest.repos.compareCommits({
+    owner,
+    repo: name,
+    base: previousCommit,
+    head: latestCommit,
+  });
+
+  console.log({ compareCommits });
 };
