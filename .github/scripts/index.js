dbab8a3d84a201ef7925122fd60f64de323327a9
Brandon Roffis <brandon.roffis@ferguson.com>
2022-05-05 15:34:50 -0400

    passing in env

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -1,4 +1,4 @@
-module.exports = async ({ github, context }) => {
+module.exports = async ({ github, context, env }) => {
   // console.log({ github });
   // console.log({ ...context.payload.repository });
 
